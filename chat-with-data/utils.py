00624ed9c51cb6e5cd4d52cdc11399b79011a29b
Sahil Jatoi <89636837+SahilJatoi744@users.noreply.github.com>
2023-08-01 16:33:22 +0500

    Update utils.py

--- chat-with-data/utils.py
+++ chat-with-data/utils.py
@@ -8,7 +8,7 @@ import streamlit as st
 from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
 from langchain.llms import OpenAI
 
-#openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = st.secrets["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"]
 
 
 def get_answer_csv(file: TextIO, query: str) -> str:
