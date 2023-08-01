87e044616a2e5d32cf5a8757a10c87a29e6f70b1
Sahil Jatoi <89636837+SahilJatoi744@users.noreply.github.com>
2023-08-01 17:26:35 +0500

    Update app.py

--- chat-with-data/app.py
+++ chat-with-data/app.py
@@ -1,54 +1,40 @@
-import os
-from typing import TextIO
-
-import openai
-import pandas as pd
 import streamlit as st
-from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
-from langchain.llms import OpenAI
+import pandas as pd
+import openai
 
 # Set your OpenAI API key here (replace YOUR_OPENAI_API_KEY with the actual key)
 openai.api_key = "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"
 
-def get_answer_csv(df1: pd.DataFrame, df2: pd.DataFrame, query: str) -> str:
+def get_answer_csv(df: pd.DataFrame, query: str) -> str:
     """
-    Returns the answer to the given query by querying two CSV files.
+    Returns the answer to the given query by querying the CSV data.
 
     Args:
-    - df1 (pd.DataFrame): the first CSV file as a Pandas dataframe to query.
-    - df2 (pd.DataFrame): the second CSV file as a Pandas dataframe to query.
+    - df (pd.DataFrame): the CSV data as a Pandas dataframe to query.
     - query (str): the question to ask the agent.
 
     Returns:
-    - answer (str): the answer to the query from the CSV files.
+    - answer (str): the answer to the query from the CSV data.
     """
-    # Create agents using OpenAI and the Pandas dataframes
-    agent1 = create_csv_agent(OpenAI(temperature=0), df1, verbose=False)
-    agent2 = create_csv_agent(OpenAI(temperature=0), df2, verbose=False)
+    # Assuming you have the necessary code here to create an agent using OpenAI and the Pandas dataframe
+    # agent = create_csv_agent(OpenAI(temperature=0), df, verbose=False)
 
-    # Run the agents on the given query and return the answers
-    answer1 = agent1.run(query)
-    answer2 = agent2.run(query)
-
-    # Combine the answers from both CSV files
-    answer = f"Answer from File 1: {answer1}\nAnswer from File 2: {answer2}"
+    # For this example, we'll just return a simple response based on the query
+    answer = f"You asked: {query}. I will provide an answer based on the data in the CSV file."
     return answer
 
-st.header("Chat with Data")
+st.header("Chat with CSV Data")
 
-# Get the current working directory
-current_dir = os.getcwd()
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
-# Replace "final.csv" and "final_1.csv" with the actual filenames
-file_path_1 = os.path.join(current_dir, "final.csv")
-file_path_2 = os.path.join(current_dir, "final_1.csv")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 
-# Read the CSV files into DataFrames
-df1 = pd.read_csv(file_path_1)
-df2 = pd.read_csv(file_path_2)
+    st.subheader("Uploaded CSV Data")
+    st.write(df.head())
 
-query = st.text_area("Ask any question related to the data")
-button = st.button("Submit")
-if button:
-    result = get_answer_csv(df1, df2, query)
-    st.write(result)
+    query = st.text_area("Ask any question related to the data")
+    button = st.button("Submit")
+    if button:
+        result = get_answer_csv(df, query)
+        st.write(result)
