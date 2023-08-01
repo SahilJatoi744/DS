8a067d8fc413a0e2beaf07a18c6671857a340a69
Sahil Jatoi <89636837+SahilJatoi744@users.noreply.github.com>
2023-08-01 17:05:36 +0500

    Update app.py

--- chat-with-data/app.py
+++ chat-with-data/app.py
@@ -1,11 +1,42 @@
-# type: ignore
-import streamlit as st
-import pandas as pd
+import os
+from typing import TextIO
 
-from utils import get_answer_csv
+import openai
+import pandas as pd
+import streamlit as st
+from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
+from langchain.llms import OpenAI
+
+# Set your OpenAI API key here (replace YOUR_OPENAI_API_KEY with the actual key)
+openai.api_key = "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"
+
+def get_answer_csv(df1: pd.DataFrame, df2: pd.DataFrame, query: str) -> str:
+    """
+    Returns the answer to the given query by querying two CSV files.
+
+    Args:
+    - df1 (pd.DataFrame): the first CSV file as a Pandas dataframe to query.
+    - df2 (pd.DataFrame): the second CSV file as a Pandas dataframe to query.
+    - query (str): the question to ask the agent.
+
+    Returns:
+    - answer (str): the answer to the query from the CSV files.
+    """
+    # Create agents using OpenAI and the Pandas dataframes
+    agent1 = create_csv_agent(OpenAI(temperature=0), df1, verbose=False)
+    agent2 = create_csv_agent(OpenAI(temperature=0), df2, verbose=False)
+
+    # Run the agents on the given query and return the answers
+    answer1 = agent1.run(query)
+    answer2 = agent2.run(query)
+
+    # Combine the answers from both CSV files
+    answer = f"Answer from CSV File 1: {answer1}\nAnswer from CSV File 2: {answer2}"
+    return answer
 
 st.header("Chat with CSV Data")
 
+# Replace "path_to_csv_file_1" and "path_to_csv_file_2" with the actual paths of your CSV files
 df1 = pd.read_csv("final.csv")
 df2 = pd.read_csv("final_1.csv")
 
