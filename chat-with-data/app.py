e32b728b842be402abb818d21ba4707af77e06c0
Sahil Jatoi <89636837+SahilJatoi744@users.noreply.github.com>
2023-08-01 16:36:01 +0500

    Update app.py

--- chat-with-data/app.py
+++ chat-with-data/app.py
@@ -1,13 +1,16 @@
 # type: ignore
 import streamlit as st
+import pandas as pd
 
 from utils import get_answer_csv
 
-st.header("Chat with any CSV")
-uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+st.header("Chat with CSV Data")
 
-if uploaded_file is not None:
-    query = st.text_area("Ask any question related to the document")
-    button = st.button("Submit")
-    if button:
-        st.write(get_answer_csv(uploaded_file, query))
+df1 = pd.read_csv("final.csv")
+df2 = pd.read_csv("final_1.csv")
+
+query = st.text_area("Ask any question related to the CSV data")
+button = st.button("Submit")
+if button:
+    result = get_answer_csv(df1, df2, query)
+    st.write(result)
