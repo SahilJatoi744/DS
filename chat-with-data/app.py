12a65ea4f0889e5eb2c0ffa75444b6b837e9a4af
Sahil Jatoi <89636837+SahilJatoi744@users.noreply.github.com>
2023-08-04 11:07:45 +0500

    Update app.py

--- chat-with-data/app.py
+++ chat-with-data/app.py
@@ -1,63 +1,27 @@
-# type: ignore
-import os
-from typing import TextIO
 import streamlit as st
-
-import openai
-import pandas as pd
-import streamlit as st
-from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
-from langchain.llms import OpenAI
-
-# Retrieve the OpenAI API key from the Streamlit secrets manager
-st.sidebar.markdown("### OpenAI API Key")
-openai_api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
-
-# Check if the API key starts with "sk-"
-if openai_api_key.strip().startswith("sk-"):
-    OPENAI_API_KEY = openai_api_key.strip()
-    # Configure OpenAI API
-    openai.api_key = OPENAI_API_KEY
-    proceed = True
-else:
-    proceed = False
-    st.sidebar.warning("OpenAI API key should start with 'sk-'")
-
-OPENAI_API_KEY = openai_api_key
+from streamlit_chat import message
+from langchain.document_loaders import UnstructuredCSVLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.indexes import VectorstoreIndexCreator
 
 
-def get_answer_csv(file: TextIO, query: str) -> str:
-    """
-    Returns the answer to the given query by querying a CSV file.
-
-    Args:
-    - file (str): the file path to the CSV file to query.
-    - query (str): the question to ask the agent.
-
-    Returns:
-    - answer (str): the answer to the query from the CSV file.
-    """
-    # Load the CSV file as a Pandas dataframe
-    # df = pd.read_csv(file)
-    #df = pd.read_csv("titanic.csv")
-
-    # Create an agent using OpenAI and the Pandas dataframe
-    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=False)
-    #agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
-
-    # Run the agent on the given query and return the answer
-    #query = "whats the square root of the average age?"
-    answer = agent.run(query)
-    return answer
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+data = UnstructuredCSVLoader('final.csv')
+index = VectorstoreIndexCreator().from_loaders([data])
 
 
-st.header("Chat with any CSV")
-uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+def chat():
+    question = st.text_input('Enter your query:')
+    api_key = st.sidebar.text_input('API Key')
 
-if uploaded_file is not None:
-    query = st.text_area("Ask any question related to the document")
-    button = st.button("Submit")
-    if button:
-        st.write(get_answer_csv(uploaded_file, query))
+    if question != None and api_key != None:
+        if api_key.startswith('sk-'):
+            response = index.query(llm=llm, question=question, chain_type="stuff", api_key=api_key)
+            message("Bot:", response)
+        else:
+            st.error('API key must start with "sk-"')
 
 
+st.title('Chat your data')
+
+chat()
