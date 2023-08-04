1d6201865062dc27bf3d10e366bc7d5f359e3ff4
Sahil Jatoi <89636837+SahilJatoi744@users.noreply.github.com>
2023-08-04 12:42:55 +0500

    Update app.py

--- chat-with-data/app.py
+++ chat-with-data/app.py
@@ -5,17 +5,16 @@ from langchain.chat_models import ChatOpenAI
 from langchain.indexes import VectorstoreIndexCreator
 
 
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-data = UnstructuredCSVLoader('final.csv')
-index = VectorstoreIndexCreator().from_loaders([data])
-
-
 def chat():
     question = st.text_input('Enter your query:')
     api_key = st.sidebar.text_input('API Key')
 
     if question != None and api_key != None:
         if api_key.startswith('sk-'):
+            llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+            data = UnstructuredCSVLoader('final.csv')
+            index = VectorstoreIndexCreator().from_loaders([data])
+
             response = index.query(llm=llm, question=question, chain_type="stuff", api_key=api_key)
             message("Bot:", response)
         else:
